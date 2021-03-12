c075b1dd1e7ed388e721ee3d031fc77a8682719e
Adina CHirila <47609475+Galssy@users.noreply.github.com>
2021-03-12 21:34:28 +0200

    added JS functionality

--- script.js
+++ script.js
@@ -5,42 +5,14 @@ const secsDisplay = document.querySelector(".secs-left");
 const startCountdownBtn = document.querySelector(".start-countdown-btn");
 const modal = document.querySelector(".modal-form");
 const backdrop = document.querySelector(".backdrop");
-
-//here or in the function?
+const settingsBtn = document.querySelector(".settings");
 const dateInput = document.querySelector("#date-input");
 const timeInput = document.querySelector("#time-input");
 
-// var m = moment("2011-10-10T10:20:90");
-// console.log(m.isValid()); // false
-// console.log(m.invalidAt()); // 5 for seconds
-
-const getInputValue = (input) => {
-	return input.value;
-};
-
 const setLaunch = () => {
-	const launchDate = getInputValue(dateInput);
-	console.log(launchDate);
-
-	const launchTime = getInputValue(timeInput);
-	console.log(launchTime);
-
+	const launchDate = dateInput.value;
+	const launchTime = timeInput.value;
 	const launch = moment(`${launchDate} ${launchTime}`);
-
-	if (!moment(launchTime, "HH:mm", true).isValid()) {
-		console.log("time not valid");
-		return;
-	}
-
-	// const testIfValid = moment(launch, "YYYY-MM-DD HH:mm", true).isValid();
-	// console.log(testIfValid);
-	// const launch = moment("2021-03-20 22:00:00");
-	// console.log(launch.isValid());
-
-	// if (!moment(launch, "YYYY-MM-DD HH:mm", true).isValid()) {
-	// 	console.log("not valid");
-	// 	return;
-	// }
 
 	return launch;
 };
@@ -50,31 +22,17 @@ const setDateAndTimeInputs = () => {
 	const timeInput = document.querySelector("#time-input");
 
 	const now = moment();
-	dateInput.setAttribute("min", now.add(1, "d").format("YYYY-MM-DD"));
-	dateInput.value = now.format("YYYY-MM-DD");
-	// dateInput.setAttribute("max", now.format("YYYY-MM-DD"));
+	dateInput.value = now.add(1, "d").format("YYYY-MM-DD");
+	dateInput.setAttribute("min", now.format("YYYY-MM-DD"));
+	dateInput.setAttribute("max", now.endOf("month").format("YYYY-MM-DD"));
 
-	timeInput.value = now.format("HH:mm");
-	// timeInput.setAttribute("min", now.add(1, "h").format("HH:mm"));
-	// console.log(now.add(1, "h"));
-	// console.log(now.add(1, "h").format("HH:mm"));
+	timeInput.value = moment().format("HH:mm");
 };
 
-const hideModalAndBackdrop = () => {
-	modal.style.display = "none";
-	backdrop.style.display = "none";
+const handleModalAndBackdrop = (display) => {
+	modal.style.display = display;
+	backdrop.style.display = display;
 };
-
-//take values from input
-//check if:
-//input is not empty
-//the date is launch valid
-//the date launch date is after the present moment
-// moment().isValid();
-//Additionally, you can use moment#invalidAt to determine which date unit overflowed.
-//m.invalidAt(); // 5 for seconds
-// const checkIfAfter = moment(launch).isAfter(now);
-// console.log(checkIfAfter);
 
 const formatTime = (time) => {
 	let formattedTime = "0";
@@ -93,13 +51,23 @@ const updateCountdown = (days, hours, mins, secs) => {
 	secsDisplay.textContent = formatTime(secs);
 };
 
+const manageCountdownDisplay = (display, titleText) => {
+	const countdown = document.querySelector(".countdown");
+	countdown.style.display = display;
+	const title = document.querySelector(".title");
+	title.innerText = titleText;
+};
+
 const calculatePeriodLeft = () => {
 	const now = moment();
 	const launch = setLaunch();
-	// const launch = moment("2021-03-12 22:00:00");
-	console.log(launch);
 
 	const duration = moment.duration(launch.diff(now));
+	if (duration < 0) {
+		manageCountdownDisplay("none", "LAUNCH IS OVER!");
+		return;
+	}
+
 	const daysLeft = duration.days();
 	const hoursLeft = duration.hours();
 	const minsLeft = duration.minutes();
@@ -108,8 +76,6 @@ const calculatePeriodLeft = () => {
 	updateCountdown(daysLeft, hoursLeft, minsLeft, secsLeft);
 };
 
-// setInterval(calculatePeriodLeft, 1000);
-
 document.addEventListener("DOMContentLoaded", () => {
 	updateCountdown("0", "0", "0", "0");
 	setDateAndTimeInputs();
@@ -117,9 +83,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 startCountdownBtn.addEventListener("click", (e) => {
 	e.preventDefault();
-	hideModalAndBackdrop();
+	handleModalAndBackdrop("none");
+	calculatePeriodLeft();
+	manageCountdownDisplay("flex", "WE'RE LAUNCHING SOON");
 	setInterval(calculatePeriodLeft, 1000);
 });
 
-//time zone?
-//modal to enter launch date
+settingsBtn.addEventListener("click", () => {
+	handleModalAndBackdrop("flex");
+});
